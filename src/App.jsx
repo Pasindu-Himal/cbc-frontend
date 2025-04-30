c104c1810c27a4855a0a93a188ceefee0244a324
Pasindu-Himal <pasinduhimal996@gmail.com>
2025-04-30 16:21:13 +0530

    initializing react-dom🧨

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
 import Header from './components/header'
 import ProductCard from './components/productCard'
+import HomePage from './pages/home'
+import LoginPage from './pages/login'
+import SignUpPage from './pages/signup'
+import AdminPage from './pages/adminPage'
 
 function App() {
 
   return (
-    <>
-      <Header/>
-      <ProductCard name="Apple Laptop" description="latest product on market" price="1000/=" picture={"https://picsum.photos/id/2/200/300"} />
-      <ProductCard/>
-
-    </>
+    <BrowserRouter>
+      <div>
+        {/* <Header/> */}
+        <Routes path="/*">
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/signup" element={<SignUpPage/>}/>
+          <Route path="/admin/*" element={<AdminPage/>}/>
+          <Route path="/*" element={<h1>404 Not Found</h1>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import UserData from "./userData";
 
 export default function Header(){
     console.log("Header component loading...")
     return(
-        <div className=" bg-[#FFF70]" >
-            <h1 className="font-bold text-[100px] text-blue-400 ">Crystal Beauty Clear</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, nisi?</p>
-            <UserData></UserData>
+        <div className=" bg-yellow-400" >
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
         </div>
     )
 }
