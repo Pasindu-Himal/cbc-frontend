912ce7050d27a5d80d546183a37ad6954171c887
Pasindu-Himal <pasinduhimal996@gmail.com>
2025-05-13 17:39:29 +0530

    added useEffect hook✏️

---
+++ src/assets/sampleData.js
@@ -0,0 +1,60 @@
+export const sampleProducts = [
+
+    {
+        "_id": "67f6489a8827209dc9353a38",
+        "productId": "COSM24001a",
+        "name": "Face Creamzzzz",
+        "altNames": [
+            "Moisture Boosterzzz",
+            "Glow Serum"
+        ],
+        "description": "Whitenining Serum that deeply brighten the skinzzzz",
+        "images": [
+            "",
+            ""
+        ],
+        "labelledPrice": 25.999999,
+        "price": 20.9999999,
+        "stock": 1200,
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "67f6489a8827209dc9353a38",
+        "productId": "COSM24001b",
+        "name": "Face Creamzzzz",
+        "altNames": [
+            "Moisture Boosterzzz",
+            "Glow Serum"
+        ],
+        "description": "Whitenining Serum that deeply brighten the skinzzzz",
+        "images": [
+            "",
+            ""
+        ],
+        "labelledPrice": 25.999999,
+        "price": 20.9999999,
+        "stock": 1200,
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "67f6489a8827209dc9353a38",
+        "productId": "COSM24001c",
+        "name": "Face Creamzzzz",
+        "altNames": [
+            "Moisture Boosterzzz",
+            "Glow Serum"
+        ],
+        "description": "Whitenining Serum that deeply brighten the skinzzzz",
+        "images": [
+            "",
+            ""
+        ],
+        "labelledPrice": 25.999999,
+        "price": 20.9999999,
+        "stock": 1200,
+        "isAvailable": true,
+        "__v": 0
+    }
+]
--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,7 +1,76 @@
+import { useEffect, useState } from "react"
+import { sampleProducts } from "../../assets/sampleData"
+import axios from "axios"
+
 export default function AdminProductsPage(){
+
+    const [products, setProducts] = useState(sampleProducts)
+
+    useEffect(
+        () => {
+            axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products").then((res) => {
+                console.log(res.data)
+                setProducts(res.data)
+            });
+        },[]
+    )
+
+    
     return(
-        <div className="w-full h-full bg-yellow-400 max-h-full overflow-y-scroll">
-            <div className="w-40px h-[1600px] border-[5px] border-blue-900"></div>
+        <div className="w-full h-full max-h-full overflow-y-scroll">
+        <table className="w-full text-center">
+            <thead>
+                <tr>
+                    <th>Product Id</th>
+                    <th>Name</th>
+                    <th>Image</th>
+                    <th>Labelled Price</th>
+                    <th>Price</th>
+                    <th>Stock</th>
+                </tr>
+            </thead>
+            <tbody>
+                {/* <tr>
+                    <td>COSM24001a</td>
+                    <td>Charcoal Face Mask</td>
+                    <td><img src="https://example.com/images/charcoal_mask.jpg" className="w-[50px] h-[50px]" /></td>
+                    <td>20</td>
+                    <td>16.75</td>
+                    <td>60</td>
+                </tr>
+                <tr>
+                    <td>COSM24001a</td>
+                    <td>Charcoal Face Mask</td>
+                    <td><img src="https://example.com/images/charcoal_mask.jpg" className="w-[50px] h-[50px]" /></td>
+                    <td>20</td>
+                    <td>16.75</td>
+                    <td>60</td>
+                </tr> */}
+
+                {
+                    products.map(
+                        (item,index)=>{
+                        // console.log("Hi")
+                        return(
+                            // <span>Hi</span>
+                            // <span>{item.productId}</span>
+                            // <span key={index}>{index}</span>
+
+                            <tr key={index}>
+                                <td>{item.productId}</td>
+                                <td>{item.name}</td>
+                                <td><img src={item.images[0]} className="w-[50px] h-[50px]" /></td>
+                                <td>{item.labelledPrice}</td>
+                                <td>{item.price}</td>
+                                <td>{item.stock}</td>
+                            </tr>
+
+                        )
+                    })
+                }
+
+            </tbody>
+        </table>
         </div>
     )
 }
